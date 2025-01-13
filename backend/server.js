68192e259076772a55b467bc61ad5be0625080c6
RyneeeJ <imrynegandia@gmail.com>
2025-01-14 01:09:08 +0800

    Connect mongoDB via mongoose

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-
-const port = process.env.PORT || 3000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
